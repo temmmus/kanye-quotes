310cff308f20c212467315463bd6b9155e20b8ee
temmmus <temmmus@gmail.com>
2024-09-12 23:15:00 +0700

    refactor: move bouncing head from index.html to App component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<template>
-  <Quotes />
-  <!-- <BouncingHead /> -->
-</template>
-
 <script setup lang="ts">
 import Quotes from "./components/Quotes.vue";
-// import BouncingHead from "./components/BouncingHead.vue";
+import BouncingHead from "./components/BouncingHead.vue";
 </script>
+
+<template>
+  <Quotes />
+  <BouncingHead />
+</template>
 
 <style scoped></style>
