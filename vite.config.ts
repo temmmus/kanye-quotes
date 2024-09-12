8b7275ce177c0d5491fc51ffaf3257b403227a96
temmmus <temmmus@gmail.com>
2024-09-12 23:13:14 +0700

    chore: add deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  base: "/kanye-quotes/",
+  plugins: [vue(), ghPages()],
+});
